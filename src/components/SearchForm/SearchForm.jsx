23f25f34a3cff162f99fe4e42647138831540010
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-05-13 11:21:28 +0300

    add form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,38 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    value: '',
+  };
+
+  onChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    if (!this.state.value) {
+      alert('Введіть текс для пошуку! ');
+      return;
+    }
+    this.props.handleSubmit(this.state.value);
+    this.setState({ value: '' });
+  };
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.onSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          value={this.state.value}
+          onChange={this.onChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,27 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    value: '',
+    page: 1,
+    items: [],
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const { value, page } = this.state;
+    if (prevState.value !== value || prevState.page !== page) {
+      ImageService.getImages(value, page).then(data => {
+        console.log(data);
+      });
+    }
+  }
+
+  handleSubmit = value => {
+    this.setState({ value });
+  };
   render() {
     return (
       <>
+        <SearchForm handleSubmit={this.handleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
