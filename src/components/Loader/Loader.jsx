ed15f4074d58155d7694bf2830034ffd8cad1c1b
MargaritaSukhova <margarytasu@gmail.com>
2023-05-13 12:18:30 +0300

    task-3

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,25 @@
+import { FallingLines } from 'react-loader-spinner';
+
+export const Loader = () => {
+  return (
+    <div
+      styles={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <FallingLines
+        color="#4fa94d"
+        width="100"
+        visible={true}
+        ariaLabel="falling-lines-loading"
+      />
+    </div>
+  );
+};
